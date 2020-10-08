d8cbef60c508cf14fb812d1f4a9f09e2ec6158de
oscar <oscarada87@gmail.com>
2020-10-08 10:50:50 +0800

    修改藍新金流API網址

--- lib/offsite_payments/integrations/spgateway.rb
+++ lib/offsite_payments/integrations/spgateway.rb
@@ -29,11 +29,11 @@ module OffsitePayments #:nodoc:
         mode = ActiveMerchant::Billing::Base.mode
         case mode
           when :production
-            'https://core.spgateway.com/MPG/mpg_gateway'
+            'https://core.newebpay.com/MPG/mpg_gateway'
           when :development
-            'https://ccore.spgateway.com/MPG/mpg_gateway'
+            'https://ccore.newebpay.com/MPG/mpg_gateway'
           when :test
-            'https://ccore.spgateway.com/MPG/mpg_gateway'
+            'https://ccore.newebpay.com/MPG/mpg_gateway'
           else
             raise StandardError, "Integration mode set to an invalid value: #{mode}"
         end
